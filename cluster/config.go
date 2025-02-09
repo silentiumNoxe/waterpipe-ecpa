3abadef7eed15138349a037c6a015da0fb74de33
silentium.noxe <silentium.noxe@gmail.com>
2025-02-09 14:29:26 +0200

    godoc

--- cluster/config.go
+++ cluster/config.go
@@ -8,16 +8,37 @@ import (
 )
 
 type Config struct {
+	// ClusterId separate replicas by cluster
 	ClusterId byte
+
+	// ReplicaId server id in cluster network
 	ReplicaId uint32
-	Out       Outcome
-	DB        sm.MessageDB
-	Addr      *net.UDPAddr
 
-	Peers     map[uint32]string
-	Port      string
+	// Out
+	// Deprecated
+	Out Outcome
+
+	// DB Database of messages in cluster
+	DB sm.MessageDB
+
+	// Addr host address
+	Addr *net.UDPAddr
+
+	// Peers members in cluster. You can pre-define member on startup
+	//
+	// server will ping peers with heathbeat. This action register the server in cluster for other members
+	Peers map[uint32]string
+
+	// Port
+	// Deprecated
+	Port string
+
+	// WaitGroup cluster uses goroutines. Required for graceful shut down
 	WaitGroup *sync.WaitGroup
-	Logger    *slog.Logger
 
+	// Logger set for customization logging
+	Logger *slog.Logger
+
+	// Secret required for auth in cluster. All members must have the same secret
 	Secret string
 }
